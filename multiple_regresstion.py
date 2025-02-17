76fbc23d0d05b9193378b602f1b7d8564697e41f
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-17 16:05:41 +0700

    Update multiple_regresstion.py

--- multiple_regresstion.py
+++ multiple_regresstion.py
@@ -17,6 +17,11 @@ st.write(df)
 # Tiền xử lý dữ liệu
 
 # Chia tập dữ liệu
+# đưa dữ liệu vào X và y
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+
+# chia dữ liệu thành train 70, test 15, valid 15
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
 X_valid, X_test, y_valid, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
