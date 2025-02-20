88a8f9c634c806c25958b521d1b45d8009068b52
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-20 20:09:43 +0700

    Update MNIST.py

---
+++ MNIST.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np # type: ignore
+
+# Load MNIST dataset
+mnist = fetch_openml('mnist_784', version=1)
+X, y = mnist["data"], mnist["target"]
+y = y.astype(np.uint8)
+
+# Streamlit app
+st.title("MNIST Classification with Streamlit & MLFlow")
